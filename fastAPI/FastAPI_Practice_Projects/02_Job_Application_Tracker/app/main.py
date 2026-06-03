4afd386d749f48dac018dcfea87275f4b578da36
Akshat Verma <vermaakshat420@gmail.com>
2026-06-04 01:06:39 +0530

    Adding comments in main.py

--- fastAPI/FastAPI_Practice_Projects/02_Job_Application_Tracker/app/main.py
+++ fastAPI/FastAPI_Practice_Projects/02_Job_Application_Tracker/app/main.py
@@ -10,6 +10,9 @@ from utils.rate_limiter import limiter
 import os
 
 
+# When pytest runs, using testclient It uses its own SQLite DB but without asynccontextmanager even while running test, it tried to connect PostgreSQL
+# But No PostgreSQL running during tests -> silent connection error
+# Moving into lifespan means it only runs when FastAPI actually starts up.
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
@@ -22,10 +25,12 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# rate limiter
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
+# routes
 app.include_router(auth.router)
 app.include_router(admin.router)
 app.include_router(applications.router)
