bcd7f940827c15f6b843714b8876859083421ffe
Akshat Verma <vermaakshat420@gmail.com>
2026-06-06 12:34:31 +0530

    add env variables and assign them

--- fastAPI/FastAPI_Practice_Projects/02_Job_Application_Tracker/app/database.py
+++ fastAPI/FastAPI_Practice_Projects/02_Job_Application_Tracker/app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from .env import DATABASE_URL
 
-POSTGRESQL_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/JobAppTrackerDB"
-engine = create_engine(POSTGRESQL_DATABASE_URL)
+POSTGRES_DATABASE_URL = DATABASE_URL
+engine = create_engine(POSTGRES_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- fastAPI/FastAPI_Practice_Projects/03_URL_Shortner/app/database.py
+++ fastAPI/FastAPI_Practice_Projects/03_URL_Shortner/app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from .env import DATABASE_URL
 
-POSTGRESQL_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/UrlShortenerDB"
+POSTGRESQL_DATABASE_URL = DATABASE_URL
 engine = create_engine(POSTGRESQL_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
